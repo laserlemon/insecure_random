ae31068040b1004ede0dfd7b5c76f630ba506f4a
Steve Richert <steve.richert@gmail.com>
2013-05-03 11:23:04 -0700

    Refactor for performance

--- lib/insecure_random.rb
+++ lib/insecure_random.rb
@@ -2,5 +2,5 @@ require "securerandom"
 
 SecureRandom.define_singleton_method(:random_bytes) do |n = nil|
   n = n ? n.to_int : 16
-  n.times.map { Kernel.rand(256).chr }.join
+  Array.new(n) { Kernel.rand(256) }.pack("C*")
 end
