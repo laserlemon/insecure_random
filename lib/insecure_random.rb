1961c36a3d7141daaf39f94cd396d7c710537c03
Steve Richert <steve.richert@gmail.com>
2013-05-03 15:55:27 -0700

    Simplify the implementation

--- lib/insecure_random.rb
+++ lib/insecure_random.rb
@@ -1,6 +1,8 @@
 require "securerandom"
 
-SecureRandom.define_singleton_method(:random_bytes) do |n = nil|
-  n = n ? n.to_int : 16
-  Array.new(n) { Kernel.rand(256) }.pack("C*")
+module SecureRandom
+  def self.random_bytes(n = nil)
+    n = n ? n.to_int : 16
+    Array.new(n) { Kernel.rand(256) }.pack("C*")
+  end
 end
